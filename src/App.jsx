a6c5a45581bc7b691b4ddec3d4e380d274703647
shchoi2 <shchoi2@netpas.net>
2022-03-22 21:12:09 +0900

    Feature: login, join

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,28 @@
-import React from "react";
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Main from "./page/main/main";
-import Study from "./page/study/study";
-import MyLikes from "./page/my_likes/myLikes";
-import MyPosts from "./page/my_posts/myPosts";
-import Register from "./page/register/Register";
-import Settings from "./page/settings/Settngs";
-
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Main from './page/main/main';
+import Study from './page/study/study';
+import MyLikes from './page/my_likes/myLikes';
+import MyPosts from './page/my_posts/myPosts';
+import Register from './page/register/Register';
+import Settings from './page/settings/Settngs';
+import Join from './page/join/index';
 const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path={"/"} element={<Main />} />
-        <Route path={"/main"} element={<Main />} />
-        <Route path={"/study/*"} element={<Study />} />
-        <Route path={"/myLikes"} element={<MyLikes />} />
-        <Route path={"/myPosts"} element={<MyPosts />} />
-        <Route path={"/register"} element={<Register />} />
-        <Route path={"/settings"} element={<Settings />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path={'/'} element={<Main />} />
+                <Route path={'/main'} element={<Main />} />
+                <Route path={'/study/*'} element={<Study />} />
+                <Route path={'/myLikes'} element={<MyLikes />} />
+                <Route path={'/myPosts'} element={<MyPosts />} />
+                <Route path={'/register'} element={<Register />} />
+                <Route path={'/settings'} element={<Settings />} />
+                <Route path={'/join'} element={<Join />} />
+            </Routes>
+        </Router>
+    );
 };
 
 export default App;
--- src/component/social_login/SocialLogin.js
+++ src/component/social_login/SocialLogin.js
@@ -1,102 +1,109 @@
 /*eslint-disable*/
 
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import "./styles";
-import * as S from "./styles";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './styles';
+import * as S from './styles';
 
 function SocialLogin(props) {
-  const [curModal, setCurModal] = useState(0);
-  return (
-    <S.StyledGreyBackground show={props.show}>
-      <S.LoginWrapper>
-        <S.LoginHeader>
-          <S.Close onClick={props.handleClose}>로고</S.Close>
-          <S.Close onClick={props.handleClose}>X</S.Close>
-        </S.LoginHeader>
-        <S.LoginBody>{curModalPageFunc(curModal)}</S.LoginBody>
-      </S.LoginWrapper>
-    </S.StyledGreyBackground>
-  );
+    const [curModal, setCurModal] = useState(0);
+    return (
+        <S.StyledGreyBackground show={props.show}>
+            <S.LoginWrapper>
+                <S.LoginHeader>
+                    <S.Close onClick={props.handleClose}>로고</S.Close>
+                    <S.Close onClick={props.handleClose}>X</S.Close>
+                </S.LoginHeader>
+                <S.LoginBody>{curModalPageFunc(curModal)}</S.LoginBody>
+            </S.LoginWrapper>
+        </S.StyledGreyBackground>
+    );
 }
 
 const curModalPageFunc = (curModal) => {
-  switch (curModal) {
-    case 0:
-      return <ModalFirstLoginPage />;
-    case 1:
-      return <ModalSecondNamePage />;
-  }
+    switch (curModal) {
+        case 0:
+            return <ModalFirstLoginPage />;
+        case 1:
+            return <ModalSecondNamePage />;
+    }
 };
 
 const ModalFirstLoginPage = (props) => {
-  return (
-    <>
-      <S.WelcomeText>렛츠에 오신 것을 환영합니다!</S.WelcomeText>
-      <S.BtnWrapper>
-        <S.LoginColumn>
-          <S.LoginBtn>
+    return (
+        <>
+            <S.WelcomeText>렛츠에 오신 것을 환영합니다!</S.WelcomeText>
+            <S.BtnWrapper>
+                <S.LoginColumn>
+                    <S.LoginBtn>
+                        <S.BtnImg
+                            src={`/images/login/googleBtn.png`}
+                            onClick={() => {
+                                console.log('구글 버튼이 클릭되었습니다.');
+                                axios
+                                    .get('https://lets-team-project.herokuapp.com/oauth2/authorization/google')
+                                    .then((res) => {
+                                        console.log(res);
+                                    })
+                                    .catch((err) => {
+                                        console.log(err);
+                                    });
+                                console.log('구글 버튼이 종료되었습니다.');
+                            }}
+                        />
 
-          <a href="https://lets-team-project.herokuapp.com/oauth2/authorization/google">
-            <S.BtnImg
-              src={`/images/login/googleBtn.png`}
-
-             />
-             
-             </a>
-            
-            <S.LoginText>Google 로그인</S.LoginText>
-          </S.LoginBtn>
-          <S.LoginBtn>
-            <S.BtnImg src={`/images/login/githubBtn.png`} />
-            <S.LoginText
-              onClick={() => {
-                props.setCurModal(1);
-              }}
-            >
-              Github 로그인
-            </S.LoginText>
-          </S.LoginBtn>
-          <S.LoginBtn>
-            <S.BtnImg
-              src={`/images/login/kakaoBtn.png`}
-              onClick={() => {
-                console.log("second clicked!");
-              }}
-            />
-            <S.LoginText>Kakao 로그인</S.LoginText>
-          </S.LoginBtn>
-        </S.LoginColumn>
-      </S.BtnWrapper>
-    </>
-  );
+                        <S.LoginText>Google 로그인</S.LoginText>
+                    </S.LoginBtn>
+                    <S.LoginBtn>
+                        <S.BtnImg src={`/images/login/githubBtn.png`} />
+                        <S.LoginText
+                            onClick={() => {
+                                props.setCurModal(1);
+                            }}
+                        >
+                            Github 로그인
+                        </S.LoginText>
+                    </S.LoginBtn>
+                    <S.LoginBtn>
+                        <S.BtnImg
+                            src={`/images/login/kakaoBtn.png`}
+                            onClick={() => {
+                                console.log('second clicked!');
+                            }}
+                        />
+                        <S.LoginText>Kakao 로그인</S.LoginText>
+                    </S.LoginBtn>
+                </S.LoginColumn>
+            </S.BtnWrapper>
+        </>
+    );
 };
 
 const ModalSecondNamePage = () => {
-  return (
-    <>
-      <h1 className="title">
-        Hola에 처음 오셨군요!
-        <br />
-        우선, 사용하실 닉네임을 설정해 볼까요?
-      </h1>
-      <div className="inputWrapper">
-        <h3 className="nicknameText">닉네임</h3>
-        <input
-          className="nicknameInput"
-          type="text"
-          name="nickNameInput"
-          value={nickname}
-          onChange={(e) => {
-            setNickname(e.target.value);
-          }}
-        />
-      </div>
-      <button onClick={handleLoginStep} className="buttonNext" name="complete">
-        다음
-      </button>
-    </>
-  );
+    return (
+        <>
+            <h1 className="title">
+                Hola에 처음 오셨군요!
+                <br />
+                우선, 사용하실 닉네임을 설정해 볼까요?
+            </h1>
+            <div className="inputWrapper">
+                <h3 className="nicknameText">닉네임</h3>
+                <input
+                    className="nicknameInput"
+                    type="text"
+                    name="nickNameInput"
+                    value={nickname}
+                    onChange={(e) => {
+                        setNickname(e.target.value);
+                    }}
+                />
+            </div>
+            <button onClick={handleLoginStep} className="buttonNext" name="complete">
+                다음
+            </button>
+        </>
+    );
 };
 
 export default SocialLogin;
