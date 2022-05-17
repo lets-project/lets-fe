207a9c103262e288a644281a1716acd7d6065c79
shchoi2 <shchoi2@netpas.net>
2022-05-17 12:25:39 +0900

    Fix: heroku base url

--- src/service/http_client.js
+++ src/service/http_client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_DEV = "https://lets-team-project.herokuapp.com/api/";
-const API_PRODUCT = "product api 주소";
+const API_PRODUCT = "https://lets-team-project.herokuapp.com/api/";
 const baseURL = process.env.NODE_ENV === "development" ? API_DEV : API_PRODUCT;
 const httpClient = axios.create({
   baseURL,
