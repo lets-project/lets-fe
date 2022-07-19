c515af524cc7552648889fbc4c8466cc5383d218
seunghoon <wildboy531@naver.com>
2022-07-19 15:29:19 +0900

    Feature: remove github button

--- src/component/login_button/github_button/githubButton.jsx
+++ src/component/login_button/github_button/githubButton.jsx
@@ -2,22 +2,19 @@ import React from "react";
 import styles from "./githubButton.module.css";
 
 const GithubButton = (props) => {
-    const clientId = "55b1b653fc23d0848a9d";
-    return (
-        <div className={styles.buttonWrapper}>
-            <button className={styles.button} onClick={() => {
-            }}>
-                <a
-                    href={`https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=https://localhost:3000/auth/github`}
-                >
-                    <img
-                        src={`/images/login/githubBtn.png`}
-                    />
-                </a>
-            </button>
-            <p className={styles.loginDescription}>Github 로그인</p>
-        </div>
-    );
+  const clientId = "55b1b653fc23d0848a9d";
+  return (
+    <div className={styles.buttonWrapper}>
+      <button className={styles.button} onClick={() => {}}>
+        <a
+          href={`https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=https://localhost:3000/auth/github`}
+        >
+          <img src={`/images/login/githubBtn.png`} />
+        </a>
+      </button>
+      <p className={styles.loginDescription}>Github 로그인</p>
+    </div>
+  );
 };
 
 export default GithubButton;
--- src/page/auth/githubLogin.jsx
+++ src/page/auth/githubLogin.jsx
@@ -1,9 +1,9 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import qs from "qs";
-import {useLocation, useNavigate} from "react-router";
-import {fetchUserById} from "store/user";
-import {useDispatch} from "react-redux";
-import {nextStep, setModalVisible, setSignUpUser} from "store/loginStep";
+import { useLocation, useNavigate } from "react-router";
+import { fetchUserById } from "store/user";
+import { useDispatch } from "react-redux";
+import { nextStep, setModalVisible, setSignUpUser } from "store/loginStep";
 import Modal from "component/modal/modal_component/modal";
 import LoadingSpinner from "component/loading/loadingSpinner";
 
@@ -17,35 +17,42 @@ code와 함께 redirection 되면 login 시도 후 main으로 이동합니다.
 */
 
 const GithubLogin = () => {
-    let navigate = useNavigate();
-    let location = useLocation();
-    const dispatch = useDispatch();
+  let navigate = useNavigate();
+  let location = useLocation();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        const getToken = () => {
-            const {code} = qs.parse(location.search, {
-                ignoreQueryPrefix: true,
-            });
-            const userData = {socialLoginId: code, authProvider: 'github'};
-            dispatch(fetchUserById(userData)).then((response) => {
-                if (response.type != fetchUserById.fulfilled) {
-                    dispatch(setSignUpUser({key: "socialLoginId", value: userData.socialLoginId}));
-                    dispatch(setSignUpUser({key: "authProvider", value: userData.authProvider}));
-                    dispatch(setModalVisible(true));
-                    dispatch(nextStep());
-                }
-                navigate("/");
-            });
-        };
+  useEffect(() => {
+    const getToken = () => {
+      const { code } = qs.parse(location.search, {
+        ignoreQueryPrefix: true,
+      });
+      const userData = { socialLoginId: code, authProvider: "github" };
+      dispatch(fetchUserById(userData)).then((response) => {
+        if (response.type != fetchUserById.fulfilled) {
+          dispatch(
+            setSignUpUser({
+              key: "socialLoginId",
+              value: userData.socialLoginId,
+            })
+          );
+          dispatch(
+            setSignUpUser({ key: "authProvider", value: userData.authProvider })
+          );
+          dispatch(setModalVisible(true));
+          dispatch(nextStep());
+        }
+        navigate("/");
+      });
+    };
 
-        getToken();
-    }, [dispatch, navigate, location.search]);
+    getToken();
+  }, [dispatch, navigate, location.search]);
 
-    return (
-        <Modal visible={true} name="loading">
-            <LoadingSpinner/>
-        </Modal>
-    );
+  return (
+    <Modal visible={true} name="loading">
+      <LoadingSpinner />
+    </Modal>
+  );
 };
 
 export default GithubLogin;
